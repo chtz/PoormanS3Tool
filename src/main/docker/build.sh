0ab1d3c0bc7c4befa1297a51e9032580f07aad28
Chris <chtz@me.com>
2016-03-02 17:06:06 +0100

    no sudo

--- src/main/docker/build.sh
+++ src/main/docker/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 (cd ../../.. && mvn clean install)
 cp ../../../target/s3tool-0.0.1-SNAPSHOT.jar .
-sudo docker build -t chtz/s3tool .
+docker build -t chtz/s3tool .
--- src/main/docker/run.sh
+++ src/main/docker/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo docker run -t chtz/s3tool
+docker run -t chtz/s3tool
