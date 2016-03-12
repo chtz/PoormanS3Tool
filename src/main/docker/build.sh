b84922d097ee9cc78c3e3eb6e19609e5d3c33dc3
Christian Tschenett <chtz@me.com>
2016-03-12 08:56:49 +0100

    docker experiment removed

--- src/main/docker/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-(cd ../../.. && mvn clean install)
-cp ../../../target/s3tool-0.0.1-SNAPSHOT.jar .
-docker build -t chtz/s3tool .
--- src/main/docker/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -t chtz/s3tool
